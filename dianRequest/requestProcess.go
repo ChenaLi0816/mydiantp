6e4238703d221095a13ff596e6d09c8cc67e9e60
LiP <3192997488@qq.com>
2023-03-16 14:37:12 +0800

    fix a bug

--- dianRequest/requestProcess.go
+++ dianRequest/requestProcess.go
@@ -83,8 +83,11 @@ func play(writer *bufio.Writer, reader *bufio.Reader, CSeq int64, ntp int) {
 
 	fmt.Printf("即将从第%v秒开始播放...\n", ntp)
 	videoByte := []byte{48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65,
-		66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90}
+		66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87,
+		88, 89, 90, 91,92,93,94,95,96,97,98,99,100}
 	writer.Write(videoByte[ntp:])
+	writer.Write(videoByte)
+	writer.Write(videoByte)
 	writer.Flush()
 	fmt.Println("已发送视频数据")
 
